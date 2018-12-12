7f109ceb458d28e6ef50cb4a71a7de8c7143f53b
jpilosi <jpilosi1@gmail.com>
2018-12-11 21:35:38 -0500

    stopping and starting snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -59,12 +59,27 @@ def list_instances(project):
 def create_snapshots(project):
     "Create snapshots for EC2 instances"
 
+
+    print("running snapshoting")
+
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Snapshotanalyzer-3000")
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+
     return
 
 
